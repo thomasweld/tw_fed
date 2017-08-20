cd27e8e57eef25516695994b38402d420997c9f1
Weld, Thomas <thomas_weld@homedepot.com>
2017-08-20 15:22:28 -0400

    refresh

--- app/spec/fileMock.js
+++
@@ -1,9 +0,0 @@
-/**
- * GENERAL NOTES
- * @author TalkRise <admin@talkrise.com>
- * @description Test file used to declare static asset mocks for things like PNG, JPG, etc.
- *
- * @exports {string}
- */
-
-module.exports = 'test-file-stub';
