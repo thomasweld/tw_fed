8bb42446fe4a02675a0152a064d03c4e261d48b6
Weld, Thomas <thomas_weld@homedepot.com>
2017-08-27 21:09:32 -0400

    work on nav links, drawer, navbar

--- src/shared/Navigation/Navigation.jsx
+++ src/shared/Navigation/Navigation.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import AppBar from 'material-ui/AppBar';
 import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
 import { connect } from 'react-redux';
 
 import { navigationOpenDrawer, navigationCloseDrawer } from './NavigationDuck';
@@ -17,7 +19,61 @@ const Navigation = props => (
       docked={false}
       open={props.navigation.drawerOpen}
       onRequestChange={props.navigationCloseDrawer}
-    />
+    >
+      <Link to={'/'}>
+        <MenuItem onClick={props.navigationCloseDrawer}>
+          Home
+        </MenuItem>
+      </Link>
+
+      <Link to={'/admin'}>
+        <MenuItem onClick={props.navigationCloseDrawer}>
+          Admin
+        </MenuItem>
+      </Link>
+
+      <Link to={'/login'}>
+        <MenuItem onClick={props.navigationCloseDrawer}>
+          Login
+        </MenuItem>
+      </Link>
+
+      <Link to={'/signUp'}>
+        <MenuItem onClick={props.navigationCloseDrawer}>
+          SignUp
+        </MenuItem>
+      </Link>
+
+      <Link to={'/createEvent'}>
+        <MenuItem onClick={props.navigationCloseDrawer}>
+          Create Event
+        </MenuItem>
+      </Link>
+
+      <Link to={'/organizerTerms'}>
+        <MenuItem onClick={props.navigationCloseDrawer}>
+          Organizer Terms
+        </MenuItem>
+      </Link>
+
+      <Link to={'/userTerms'}>
+        <MenuItem onClick={props.navigationCloseDrawer}>
+          User Terms
+        </MenuItem>
+      </Link>
+
+      <Link to={'/contactUs'}>
+        <MenuItem onClick={props.navigationCloseDrawer}>
+          ContactUs
+        </MenuItem>
+      </Link>
+
+      <Link to={'/aboutUs'}>
+        <MenuItem onClick={props.navigationCloseDrawer}>
+          About Us
+        </MenuItem>
+      </Link>
+    </Drawer>
   </div>
 );
 
